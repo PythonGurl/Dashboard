af4bd0d8c59b1ba604f2e03759d8794f622db432
Alyssa2434 <alyssa.wzp@gmail.com>
2022-05-21 20:23:54 -0400

    Add read_data.py

---
+++ read_data.py
@@ -0,0 +1,5 @@
+import pandas as pd 
+median_asking_rent = pd.read_csv('medianAskingRent_All.csv')
+discount_share = pd.read_csv('discountShare_All.csv')
+rental_index = pd.read_csv('rentalIndex_All.csv')
+rental_inventory = pd.read_csv('rentalInventory_All.csv')
