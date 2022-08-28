dcf8ecdbe954b25b794c17787c226275b6f29a67
Alyssa2434 <alyssa.wzp@gmail.com>
2022-08-28 00:31:36 -0400

    remove the duplicate line

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import dash_bootstrap_components as dbc
 from dash import Dash, Input, Output, dcc, html
-import dash_bootstrap_components as dbc
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.CYBORG])
 
