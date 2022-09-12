2843b506c436bc88785d8329f862bb14e03b6b7f
Alyssa2434 <alyssa.wzp@gmail.com>
2022-09-11 22:19:09 -0400

    edited function

--- mapping.py
+++ mapping.py
@@ -1,18 +1,18 @@
 import pandas as pd
 import collections
 
-df = pd.read_csv('RentData051022/medianAskingRent_All.csv')
-print(df.shape)
 
-
-def borough_area(df, key, val):
+def borough_area():
+    df = pd.read_csv('RentData051022/medianAskingRent_All.csv')
+    df.dropna()
     d = collections.defaultdict(list)
     for row in range(df.shape[0]):
-        k = df.at[row, key]
-        v = df.at[row, val]
+        k = df.at[row, 'Borough']
+        v = df.at[row, 'areaName']
         d[k].append(v)
     return d
 
 
-d = borough_area(df, 'Borough', 'areaName')
-print(d)
+if __name__ == "__main__":
+    d = borough_area()
+    print(d)
