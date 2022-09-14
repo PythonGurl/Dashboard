78f5b9799d79cd9abe953894ecc3eeb6a127d912
Alyssa2434 <alyssa.wzp@gmail.com>
2022-09-14 11:28:38 -0400

    borough, area dropdowns

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+from statistics import median
 import dash_bootstrap_components as dbc
 from dash import Dash, Input, Output, dcc, html
+import mapping
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.CYBORG])
 server = app.server
@@ -11,20 +13,27 @@ navbar = dbc.NavbarSimple(
     dark=True,
 )
 
+boroughs = mapping.borough_area()
+
 
 app.layout = html.Div([
     navbar,
     html.H2('Hello World'),
-    dcc.RadioItems(['New York City', 'Montreal', 'San Francisco'],
-                   'Montreal', id='buttons'),
-    html.Div(id='display-value')
+    dcc.Dropdown(sorted(boroughs.keys()), placeholder='Select a borough',
+                 id='borough-dropdown'),
+    dcc.Dropdown(id='areas-dropdown', placeholder='Areas')
 ])
 
 
-@ app.callback(Output('display-value', 'children'),
-               [Input('buttons', 'value')])
-def display_value(value):
-    return f'You have selected {value}'
+@ app.callback(Output('areas-dropdown', 'options'),
+               Output('areas-dropdown', 'value'),
+               Input('borough-dropdown', 'value'), prevent_initial_call=True
+               )
+def update_areas_dropdown(borough):
+    val = False
+    if borough:
+        val = boroughs[borough][0]
+    return boroughs[borough], val
 
 
 if __name__ == '__main__':
