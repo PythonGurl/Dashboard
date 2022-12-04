3e662cbed107eac03d1c89f85c5b229dcc4131ac
Alyssa2434 <alyssa.wzp@gmail.com>
2022-12-04 23:02:45 +0800

    view.py and controller.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 import dash_bootstrap_components as dbc
-import plotly.express as px
 from dash import Dash, Input, Output, dcc, html
-import mapping
-import read_data
-from layout import get_layout
+from view import get_layout
+from controller import get_callbacks
 
 app = Dash(__name__, title='New York City Rental Market',
            external_stylesheets=[dbc.themes.CYBORG])
@@ -11,52 +9,7 @@ server = app.server
 
 
 app.layout = get_layout()
-
-# Callbacks ---------------------------------------------
-
-## Rental Inverntory Line Chartn ##
-# auto update area dropdowns based on selected borough
-
-
-@app.callback(
-    Output('areas-dropdown', 'options'),
-    Output('areas-dropdown', 'value'),
-    Input('borough-dropdown', 'value'), prevent_initial_call=True
-)
-def update_areas_dropdown(borough):
-    boroughs = mapping.borough_area()
-    val = False
-    if borough:
-        val = boroughs[borough][0]
-    return boroughs[borough], val
-
-
-@app.callback(
-    Output('area-inventory-line', 'figure'),
-    Input('areas-dropdown', 'value')
-)
-def area_inventory_line(area):
-    inventory_df = read_data.get_rental_inventory()
-    df = inventory_df[inventory_df['areaName'] == area]
-    fig = px.line(df, x='date', y='value', labels={
-                  'value': 'Inventory'}, title='Rental Inventory')
-    return fig
-
-## Sunburst Chart ##
-
-
-@app.callback(
-    Output('area-inventory-sunburst', 'figure'),
-    Input('year-dropdown', 'value')
-)
-def area_inventory_sunburst(year):
-    inventory_df = read_data.get_rental_inventory()
-    df = inventory_df[(inventory_df['areaType'] != 'submarket')
-                      & (inventory_df['areaType'] != 'borough')
-                      & (inventory_df.date.dt.year == year)]
-    fig = px.sunburst(df, path=['Borough', 'areaName'], values='value')
-    return fig
-
+get_callbacks(app)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
